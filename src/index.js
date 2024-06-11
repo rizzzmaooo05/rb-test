1ea421a366aad0fcef4cd9d6d7a85ed8a0c888c7
Rizzz Maooo <rizqonmaulana05@gmail.com>
2024-06-11 20:04:03 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,39 +1,13 @@
-import RizzzBase from "rizzzbase"
 import express from "express"
 import cors from 'cors'
+import fs from 'fs'
 
-const rb = new RizzzBase()
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('api running!')
-})
-
-app.get('/:database/:table/', (req, res) => {
-  const { database } = req.params
-  const { table } = req.params
-
-  const data = rb.useDatabase(database).from(table).select('all').data
-  
-  res.send(data)
-})
-
-app.post('/add_data/:database/:table', (req, res) => {
-  const { database } = req.params
-  const { table } = req.params
-
-  try {
-    rb.useDatabase(database).from(table).addData(req.body)
-    res.send('sukses')
-  }
-  catch (error) {
-    res.send(error)
-  }
-
-})
+app.get('/', (req, res) => res.send('api runn!'))
 
 app.listen(3004, () => console.log('server runn'))
 
